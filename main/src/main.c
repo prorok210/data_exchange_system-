35076e693c4c39c239440358413d550febabce56
prorok210 <prorok210c@gmail.com>
2025-05-17 23:26:30 +0300

    Испавление пайплайна сборки

--- main/src/main.c
+++ main/src/main.c
@@ -36,7 +36,7 @@ void app_main(void) {
     ESP_LOGI(TAG, "--- ESP8266 APP STARTING ---");
 
     esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
